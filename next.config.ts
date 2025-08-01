ed73f77960710e067d2126bfe01fdc3e6bf654da
mairisdev <mairisdigital@icloud.com>
2025-08-01 20:08:21 +0300

    AddedFixes3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+    domains: ['res.cloudinary.com']
+  }
+}
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
